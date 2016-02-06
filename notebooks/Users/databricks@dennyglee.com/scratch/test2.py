301d178a40d7aa078aee282fed85ce111bb1af01
Denny Lee <denny.g.lee@gmail.com>
2016-02-06 09:48:34 -0800

    Update test2.py

---
+++ notebooks/Users/databricks@dennyglee.com/scratch/test2.py
@@ -0,0 +1,5 @@
+# Databricks notebook source exported at Sat, 6 Feb 2016 17:47:07 UTC
+dbutils.fs.put("/home/denny/1.6/lines","""
+Hello hello is it me you are looking for
+Hello how are you
+""", true)
