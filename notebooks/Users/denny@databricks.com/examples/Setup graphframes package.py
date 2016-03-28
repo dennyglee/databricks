3ec1a784fba43b8864a9c2ef0346085ba9c76e4f
Denny Lee <denny.g.lee@gmail.com>
2016-03-28 08:59:19 -0700

    Update Setup graphframes package.py

---
+++ notebooks/Users/denny@databricks.com/examples/Setup graphframes package.py
@@ -0,0 +1,22 @@
+# Databricks notebook source exported at Mon, 28 Mar 2016 15:59:19 UTC
+# MAGIC %md
+# MAGIC ## Follow the following directions to install GraphFrames Spark Package:
+# MAGIC <p>
+# MAGIC * Click ![](http://training.databricks.com/databricks_guide/databricks_home_icon.png) to go to welcome page.
+# MAGIC <p>
+# MAGIC * Click **Library** within your workspace <b>
+# MAGIC 
+# MAGIC ![](https://databricks.com/wp-content/uploads/2016/03/attach-library-1-1024x640.png)
+# MAGIC 
+# MAGIC <p>
+# MAGIC 
+# MAGIC * Within the **New Library** dialog, under **Source**, click on **Maven Coordinate**.
+# MAGIC 
+# MAGIC * Within the **Search Packages** *search bar* dialog, type “graphframes”.
+# MAGIC ![](https://databricks.com/wp-content/uploads/2016/03/attach-library-3-1024x640.png)
+# MAGIC 
+# MAGIC * Ensure you choose the correct release (1.4, 1.5, or 1.6) that corresponds to the Spark version of your cluster (which you can choose in Databricks).  For this example, we chose to run Spark 1.6 in this example.
+# MAGIC 
+# MAGIC * Click **Create Library** and you have now installed the GraphFrames package and attached it to your cluster.
+# MAGIC 
+# MAGIC * You can automatically attach this package to your cluster by clicking on the *“Attach automatically to all clusters”* checkbox
