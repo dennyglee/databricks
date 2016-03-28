c191c70557000a16f32f768287b444764679cc47
Denny Lee <denny.g.lee@gmail.com>
2016-03-28 08:59:54 -0700

    Update Import notebooks.py

---
+++ notebooks/Users/denny@databricks.com/examples/Import notebooks.py
@@ -0,0 +1,24 @@
+# Databricks notebook source exported at Mon, 28 Mar 2016 15:59:54 UTC
+# MAGIC %md
+# MAGIC ## Follow the following directions to Import Notebooks:
+# MAGIC 
+# MAGIC * Click the Import Notebook icon in the top left or right of the notebook.
+# MAGIC 
+# MAGIC ![](https://databricks.com/wp-content/uploads/2016/03/1.-Import-Notebook-e1458159716971.png)
+# MAGIC 
+# MAGIC ![](https://databricks.com/wp-content/uploads/2016/03/clear.gif)
+# MAGIC 
+# MAGIC 
+# MAGIC * **Copy** the URL from the Import Notebook dialog box.
+# MAGIC 
+# MAGIC ![](https://databricks.com/wp-content/uploads/2016/03/2.-Copy-e1458159777314.png)
+# MAGIC 
+# MAGIC ![](https://databricks.com/wp-content/uploads/2016/03/clear.gif)
+# MAGIC 
+# MAGIC 
+# MAGIC * Go to your Databricks workspace, right-click and choose the **Import**.  Click the **URL** radio button and **paste** the URL you just copied.
+# MAGIC 
+# MAGIC ![](https://databricks.com/wp-content/uploads/2016/03/3.-Paste-e1458159818875.png)
+
+# COMMAND ----------
+
