4583762e1f78464146d956ab178a4680e5cc76ae
Denny Lee <denny.g.lee@gmail.com>
2016-03-28 08:54:48 -0700

    Update SPARK-10885 PR#8950.scala

---
+++ notebooks/Users/denny@databricks.com/demo/spark 1.6/Multiple Notebooks/SPARK-10885 PR#8950.scala
@@ -0,0 +1,9 @@
+// Databricks notebook source exported at Mon, 28 Mar 2016 15:54:48 UTC
+// MAGIC %md ## [Streaming] SPARK-10885 PR#8950: Display the failed output op in Streaming 
+// MAGIC * Display the failed output op count in the batch list
+// MAGIC * Display the failure reason of output op in the batch detail page
+// MAGIC 
+// MAGIC ![Display Streaming Failed Output](https://sparkhub.databricks.com/wp-content/uploads/2015/12/Display-Failed-Output-Streaming.png)
+
+// COMMAND ----------
+
